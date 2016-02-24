86842d37bd27325249e547cdfaa2f439697f9ba7
Xiao Ling <lingxiao@seas.upenn.edu>
2016-02-24 12:46:18 -0500

    1246

--- hello/src/Main.purs
+++ hello/src/Main.purs
@@ -1,13 +1,24 @@
 module Main where
 
 import Prelude
+
+import Math
+import Math (pi,pow)
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
 
+
+diag :: Number -> Number -> Number
+diag  w h = sqrt(w*2.0 + h)
+
+
+circ :: Number -> Number
+circ r = pi * (pow r 2.0)
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+main = print (circ 1.3)
+
 
 
 
